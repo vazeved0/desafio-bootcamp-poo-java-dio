113e25df91e23074ced5ecfb84410aa743b55b3f
Vinicius Azevedo <Viniciuslldeazevedo@gmail.com>
2023-02-22 21:41:58 -0300

    Class Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.desafio.dominio.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso javascript");
+        curso2.setDescricao("descrição curso javascript");
+        curso2.setCargaHoraria(5);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setDescricao("descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("mentoria javascript");
+        mentoria2.setDescricao("descrição mentoria javascript");
+        mentoria2.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+
     }
 }
